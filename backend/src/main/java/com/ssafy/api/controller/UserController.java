4b74b405fa6b156b2f24cff46173f204994b3285
차송희 <chasonghui@naver.com>
2022-07-27 01:53:56 +0900

    Style : rest api url update

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -28,32 +28,11 @@ import java.util.List;
  */
 @Api(value = "유저 API", tags = {"User"})
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
-
-	//송희
-	//@Autowired
-	//private JavaMailSender sender;
-
-	@PostMapping()
-	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공"),
-        @ApiResponse(code = 401, message = "인증 실패"),
-        @ApiResponse(code = 404, message = "사용자 없음"),
-        @ApiResponse(code = 500, message = "서버 오류")
-    })
-	public ResponseEntity<? extends BaseResponseBody> register(
-			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-		
-		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-		User user = userService.createUser(registerInfo);
-		
-		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
-	}
 
 	//송희 0724 --------------------------------
 	@GetMapping("/profile")
