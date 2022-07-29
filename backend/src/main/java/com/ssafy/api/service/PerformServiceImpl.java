7cf60fc9b84000e0245f32e144e71bb95722ade5
chodayeon <dus6982@naver.com>
2022-07-29 13:29:36 +0900

    Feat : 공연API-전체목록조회 기능

--- backend/src/main/java/com/ssafy/api/service/PerformServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/PerformServiceImpl.java
@@ -9,16 +9,21 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service("performService")
 public class PerformServiceImpl implements PerformService {
     @Override
     public List<PerformRes> getPerformAllList() {
-        String serviceKey = "0706bcb651424a1aacad7bb9f3564895"; //나중에 다른 곳에 빼두기~
+        List<PerformRes> list = new ArrayList<>();
+
+        //나중에 다른 곳에 빼두기~
+        String serviceKey = "0706bcb651424a1aacad7bb9f3564895";
 
         try{
 
+            //원래의 주소
             //http://www.kopis.or.kr/openApi/restful/pblprfr?service={서비스키}&stdate=20160101&eddate=20160630&rows=10&cpage=1
             String path = "http://www.kopis.or.kr/openApi/restful/pblprfr?service=";
             String stdate = "&stdate=20220628";
@@ -31,29 +36,33 @@ public class PerformServiceImpl implements PerformService {
             DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
             Document doc = dBuilder.parse(url);
 
-            // 제일 첫번째 태그
+            // 제일 첫번째 태그 (dbs)
             doc.getDocumentElement().normalize();
 
             // 파싱할 tag
             NodeList nList = doc.getElementsByTagName("db");
+            System.out.println(nList.getLength());
 
             for(int temp = 0; temp < nList.getLength(); temp++){
                 Node nNode = nList.item(temp);
 
                 Element eElement = (Element) nNode;
 
-                System.out.println("공연명 : " + getTagValue("prfnm", eElement));
-                System.out.println("공연장르명 : " + getTagValue("genrenm", eElement));
-                System.out.println("공연시작일 : " + getTagValue("prfpdfrom", eElement));
-                System.out.println("공연종료일 : " + getTagValue("prfpdto", eElement));
-                System.out.println("공연포스터경로 : " + getTagValue("poster", eElement));
-                System.out.println("공연시설명 : " + getTagValue("fcltynm", eElement));
+                //tag에 맞는 값을 res에 set
+                PerformRes res = PerformRes.of(getTagValue("prfnm", eElement), getTagValue("genrenm", eElement),
+                        getTagValue("prfpdfrom", eElement), getTagValue("prfpdto", eElement),
+                        getTagValue("poster", eElement), getTagValue("fcltynm", eElement));
+
+                //list에 res 객체 추가
+                list.add(res);
             }
 
         } catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+        
+        //공연 전체 목록 반환
+        return list;
     }
 
     // tag값의 정보를 가져오는 함수
@@ -65,23 +74,6 @@ public class PerformServiceImpl implements PerformService {
         NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
 
         result = nlList.item(0).getTextContent();
-
-        return result;
-    }
-
-    // tag값의 정보를 가져오는 함수
-    public static String getTagValue(String tag, String childTag, Element eElement) {
-
-        //결과를 저장할 result 변수 선언
-        String result = "";
-
-        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
-
-        for(int i = 0; i < eElement.getElementsByTagName(childTag).getLength(); i++) {
-
-            //result += nlList.item(i).getFirstChild().getTextContent() + " ";
-            result += nlList.item(i).getChildNodes().item(0).getTextContent() + " ";
-        }
 
         return result;
     }
